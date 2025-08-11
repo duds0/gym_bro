3b24c69844aa204e5b62af684f24489b6f09ae5d
duds0 <eduardofigueiredo1930@gmail.com>
2025-08-11 10:50:02 -0300

    Animated greetings

--- lib/ui/screens/dashboard/dashboard.dart
+++ lib/ui/screens/dashboard/dashboard.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:gym_bro/providers/workout_provider.dart';
 import 'package:gym_bro/ui/screens/dashboard/widgets/my_workouts.dart';
@@ -12,18 +14,76 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  bool _ehPreguicinha = false; //kkkk
+  late Timer _timer;
+
+  String _greeting() {
+    final hour = DateTime.now().hour;
+
+    if (hour >= 5 && hour < 12) {
+      return "Bom dia";
+    } else if (hour >= 12 && hour < 18) {
+      return "Boa tarde";
+    } else if (hour >= 18 && hour < 24) {
+      return "Boa noite";
+    } else {
+      return "Boa madrugada";
+    }
+  }
+
   @override
   void initState() {
     Future.microtask(
       () => Provider.of<WorkoutProvider>(context, listen: false).loadAll(),
     );
+
+    _timer = Timer.periodic(Duration(seconds: 15), (_) {
+      setState(() {
+        _ehPreguicinha = !_ehPreguicinha;
+      });
+    });
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Bom dia, Preguicinha!"), centerTitle: false),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Text("${_greeting()}, "),
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              transitionBuilder: (child, animation) {
+                final isEntering = child.key == ValueKey(_ehPreguicinha);
+
+                final offsetAnimation = Tween<Offset>(
+                  begin: isEntering ? const Offset(0, -1) : const Offset(0, 1),
+                  end: Offset.zero,
+                ).animate(animation);
+
+                return ClipRect(
+                  child: SlideTransition(
+                    position: offsetAnimation,
+                    child: child,
+                  ),
+                );
+              },
+              child: Text(
+                _ehPreguicinha ? "Preguicinha!" : "Campeã(o)!",
+                key: ValueKey(_ehPreguicinha),
+              ),
+            ),
+          ],
+        ),
+        centerTitle: false,
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Column(
